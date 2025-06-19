916c578e3184b7b9dd02823e6fb18c4e3c62add3
msuarz <mikemps@gmail.com>
2025-06-19 08:03:59 -0400

    Auto-commit: files changed

--- pages/index.jsx
+++ pages/index.jsx
@@ -20,7 +20,7 @@ export default function Home() {
             />
             <div style={{
               position: 'absolute',
-              top: '45%',
+              top: 'calc(45% - 10px)',
               left: '30%',
               transform: 'translate(-50%, -50%)',
               zIndex: 10,
