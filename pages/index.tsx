9abfc37df609e5b0c20d029b1a2be9f9572d2f39
msuarz <mikemps@gmail.com>
2025-06-18 22:34:53 -0400

    Add comic-style thought cloud design to homepage message

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,16 +9,55 @@ export default function Home() {
           <img style={{ float: 'left' }} src="img/minime.png" />
         </div>
         <div className="col-8">
-          <div className="alert alert-info">
-            <p><strong>Hi there, I'm Mike Suarez.</strong></p>
-            
-            <p>
-              This is my personal website, it is a collection of things I like.
-              <br/>
-              Which, in a way, should give an idea of whom I am.
-            </p>
+          <div className="position-relative">
+            <div style={{
+              background: 'white',
+              border: '3px solid #333',
+              borderRadius: '30px',
+              padding: '20px 25px',
+              marginLeft: '30px',
+              marginTop: '10px',
+              position: 'relative',
+              boxShadow: '3px 3px 0px #ccc'
+            }}>
+              {/* Cloud tail */}
+              <div style={{
+                position: 'absolute',
+                left: '-20px',
+                bottom: '30px'
+              }}>
+                <div style={{
+                  width: '15px',
+                  height: '15px',
+                  background: 'white',
+                  border: '3px solid #333',
+                  borderRadius: '50%',
+                  marginBottom: '8px'
+                }}></div>
+                <div style={{
+                  width: '10px',
+                  height: '10px',
+                  background: 'white',
+                  border: '3px solid #333',
+                  borderRadius: '50%',
+                  marginLeft: '-8px'
+                }}></div>
+              </div>
+              
+              <p style={{ margin: '0 0 15px 0', fontWeight: 'bold', fontSize: '16px' }}>
+                Hi there, I'm Mike Suarez.
+              </p>
+              
+              <p style={{ margin: '0 0 15px 0', lineHeight: '1.4' }}>
+                This is my personal website, it is a collection of things I like.
+                <br/>
+                Which, in a way, should give an idea of whom I am.
+              </p>
 
-            <p>There's a lot of computer stuff since I consider myself a natural born programmer.</p>
+              <p style={{ margin: '0', lineHeight: '1.4' }}>
+                There's a lot of computer stuff since I consider myself a natural born programmer.
+              </p>
+            </div>
           </div>
         </div>
       </div>
