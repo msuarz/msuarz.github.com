2776775a3cc4e45c8364db1b53befd33e93b6e2f
msuarz <mikemps@gmail.com>
2025-06-19 08:12:24 -0400

    Auto-commit: files changed

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,11 +9,35 @@ export default function Home() {
           <img style={{ float: 'left', marginTop: '100px', transform: 'scale(1.2)' }} src="img/minime.png" />
         </div>
         <div className="col-8">
-          <img 
-            src="/img/speech-bubble.svg" 
-            alt="speech bubble"
-            style={{ width: '441px', height: 'auto', marginTop: '-70px' }}
-          />
+          <div style={{ position: 'relative', marginTop: '-70px' }}>
+            <img 
+              src="/img/cloud9.png" 
+              alt="thought cloud"
+              style={{ width: '441px', height: 'auto' }}
+            />
+            <div style={{
+              position: 'absolute',
+              top: '40%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)',
+              zIndex: 10,
+              textAlign: 'center',
+              width: '240px',
+              padding: '10px'
+            }}>
+              <p style={{ margin: '0 0 15px 0', fontWeight: 'bold', fontSize: '16px' }}>
+                Hi there, I'm Mike Suarez.
+              </p>
+              
+              <p style={{ margin: '0 0 15px 0', fontSize: '14px', lineHeight: '1.4' }}>
+                This is my personal website, it is a collection of things I like. Which, in a way, should give an idea of whom I am.
+              </p>
+
+              <p style={{ margin: '0', fontSize: '14px', lineHeight: '1.4' }}>
+                There's a lot of computer stuff since I consider myself a natural born programmer.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </Layout>
