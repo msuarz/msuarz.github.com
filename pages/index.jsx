6518482ec4bd8b90cd935832698d8031c9198a6b
msuarz <mikemps@gmail.com>
2025-06-19 00:11:06 -0400

    Auto-commit: files changed

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,31 +9,39 @@ export default function Home() {
           <img style={{ float: 'left' }} src="img/minime.png" />
         </div>
         <div className="col-8">
-          <div style={{
-            backgroundImage: 'url(/img/cloud9.png)',
-            backgroundSize: '100% 100%',
-            backgroundRepeat: 'no-repeat',
-            backgroundPosition: 'center',
-            padding: '60px 40px',
-            minHeight: '200px',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            textAlign: 'center'
-          }}>
-            <p style={{ margin: '0 0 15px 0', fontWeight: 'bold', fontSize: '16px' }}>
-              Hi there, I'm Mike Suarez.
-            </p>
-            
-            <p style={{ margin: '0 0 15px 0', fontSize: '14px', lineHeight: '1.4' }}>
-              This is my personal website, it is a collection of things I like.
-              <br/>
-              Which, in a way, should give an idea of whom I am.
-            </p>
+          <div style={{ position: 'relative' }}>
+            <img 
+              src="/img/cloud9.png" 
+              alt="thought cloud"
+              style={{
+                width: '100%',
+                maxWidth: '400px',
+                height: 'auto'
+              }}
+            />
+            <div style={{
+              position: 'absolute',
+              top: '50%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)',
+              zIndex: 10,
+              textAlign: 'center',
+              width: '80%'
+            }}>
+              <p style={{ margin: '0 0 15px 0', fontWeight: 'bold', fontSize: '16px' }}>
+                Hi there, I'm Mike Suarez.
+              </p>
+              
+              <p style={{ margin: '0 0 15px 0', fontSize: '14px', lineHeight: '1.4' }}>
+                This is my personal website, it is a collection of things I like.
+                <br/>
+                Which, in a way, should give an idea of whom I am.
+              </p>
 
-            <p style={{ margin: '0', fontSize: '14px', lineHeight: '1.4' }}>
-              There's a lot of computer stuff since I consider myself a natural born programmer.
-            </p>
+              <p style={{ margin: '0', fontSize: '14px', lineHeight: '1.4' }}>
+                There's a lot of computer stuff since I consider myself a natural born programmer.
+              </p>
+            </div>
           </div>
         </div>
       </div>
