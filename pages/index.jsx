fd0335f32b7339d2c452542ba6178c38394ecd97
msuarz <mikemps@gmail.com>
2025-06-19 08:02:58 -0400

    Auto-commit: files changed

--- pages/index.jsx
+++ pages/index.jsx
@@ -21,11 +21,11 @@ export default function Home() {
             <div style={{
               position: 'absolute',
               top: '45%',
-              left: '35%',
+              left: '30%',
               transform: 'translate(-50%, -50%)',
               zIndex: 10,
               textAlign: 'center',
-              width: '180px',
+              width: '220px',
               padding: '10px'
             }}>
               <p style={{ margin: '0 0 15px 0', fontWeight: 'bold', fontSize: '16px' }}>
